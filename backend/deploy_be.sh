85532054b1c1257d49af4fd7babdfc5f85b84f6f
RakaFDe <rakafde@gmail.com>
2026-03-19 21:01:33 +0700

    tambah CI untuk backend

--- backend/deploy_be.sh
+++ backend/deploy_be.sh
@@ -1,6 +1,8 @@
 #pastikan env sudah sesuai
 set -e
 
+cd "$(dirname "$0")"
+
 echo "Pull repo ops terbaru"
 git pull
 
@@ -8,10 +10,10 @@ echo "Pull images"
 docker compose pull
 
 echo "Run database migration"
-docker compose --profile migration up migrate
+docker compose --profile migration up --abort-on-container-exit migrate
 
 echo "Jalankan compose"
-docker compose up -d
+docker compose up -d --remove-orphans
 
 echo "Clean unused images / Prune image"
 docker image prune -f
