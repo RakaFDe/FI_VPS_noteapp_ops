b2b6a38386f0d3342ff063510276d5b24db6ea79
RakaFDe <rakafde@gmail.com>
2026-04-10 22:46:54 +0700

    rapihkan script deploy be

--- scripts/deploy_be_k8s.sh
+++ scripts/deploy_be_k8s.sh
@@ -14,11 +14,19 @@ kubectl apply -f backend/k8s/postgres/
 echo "Wait Postgres ready"
 kubectl rollout status statefulset/finote-postgres --timeout=180s || true
 
+echo "Cleaning up old migration job if exists"
+kubectl delete job finote-migrate --ignore-not-found
+
 #echo "Run migration job"
 kubectl apply -k backend/k8s/migrate/
 
 echo "Wait migration complete"
-kubectl wait --for=condition=complete job/finote-migrate --timeout=120s || true
+if kubectl wait --for=condition=complete job/finote-migrate --timeout=120s; then
+    echo "Migration successful!"
+else
+    echo "Migration failed or timed out! Aborting deployment."
+    exit 1
+fi
 
 echo "Deploy backend"
 kubectl apply -k backend/k8s/app
