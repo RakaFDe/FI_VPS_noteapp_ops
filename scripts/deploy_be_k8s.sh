3e93ec764d6258c2cd81925acf1f1a022e02210c
rakafde <rakafde@gmail.com>
2026-04-10 15:28:16 +0000

    sesuaikan perfolder backend

--- scripts/deploy_be_k8s.sh
+++ scripts/deploy_be_k8s.sh
@@ -8,22 +8,20 @@ echo "Sync repo ops terbaru"
 git fetch origin
 git reset --hard origin/main
 
-echo "Apply Postgres Secret"
-kubectl apply -f backend/k8s/postgres-secret.yaml
-
 echo "Apply Postgres"
-kubectl apply -f backend/k8s/postgres-pvc.yaml
-kubectl apply -f backend/k8s/postgres-service.yaml
-kubectl apply -f backend/k8s/postgres-statefulset.yaml
+kubectl apply -f backend/k8s/postgres/
 
 echo "Wait Postgres ready"
 kubectl rollout status statefulset/finote-postgres --timeout=180s || true
 
-echo "Run migration job"
-kubectl apply -f backend/k8s/postgres-migrate-job.yaml
+#echo "Run migration job"
+kubectl apply -f backend/k8s/migrate/
 
-echo "Deploy backend via kustomize"
-kubectl apply -k backend/k8s
+echo "Wait migration complete"
+kubectl wait --for=condition=complete job/finote-migrate --timeout=120s || true
+
+echo "Deploy backend"
+kubectl apply -k backend/k8s/app
 
 echo "Wait rollout backend"
 kubectl rollout status deployment/finote-backend
